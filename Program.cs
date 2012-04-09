026f92d56691acaeb46a2787be2ba0f8eaf36794
Zhengmin li <zhengmin.li@luxoom.cn>
2012-04-09 16:43:28 +0800

    Change directory for current project.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿// 
-// ArrayClass.cs
+// Program.cs
 //  
 // Author:
-//       Leezhm <leezhm(at)126.com>
+//       leezhm <leezhm(at)126.com>
 // 
 // Copyright (c) 2012 leezhm(at)126.com
+// 
+// Created:
+// 		 2012/4/9
+// 
+// Modified:
+// 	     leezhm <2012/4/9> 
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,67 +29,16 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-//
+// 
 
-using System;
-
-using System.Threading.Tasks;
-
-class ArrayClass
+namespace Program
 {
-	private static void PrintArray(string [] arr)
+	public class Program
 	{
-//		Parallel.For(0, arr.Length, i =>
-//		{
-//			Console.Write(arr[i] + "{0}", i < arr.Length - 1 ? " " : "\n");
-//		});
-		for(int i = 0; i < arr.Length; ++ i)
+		public static void Main()
 		{
-			Console.Write(arr[i] + "{0}", i < arr.Length - 1 ? " -> " : "\n");
+			// Chapter 1 ArrayClass
+			ArrayClass.ArrayClass.RunArrayClass();
 		}
-	}
-	
-	private static void ChangeArray(string [] arr)
-	{
-		// Reserve array
-		Array.Reverse(arr);
-		
-		Console.Write("Print Array in ChangeArray, after reversing current Array ... ");
-		PrintArray(arr);
-	}
-	
-	private static void ChangeArrayElements(string [] arr)
-	{
-		// Change array element
-		arr[0] = "Sun";
-		arr[1] = "Mon";
-		arr[2] = "Tues";
-		
-		Console.Write("Print Array in ChangeArrayElement ... ");
-		PrintArray(arr);
-	}
-	
-	public static void Main()
-	{
-		// Declare and initialize an array
-		string [] weekDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-						      "Friday", "Saturday"};
-		
-		
-		// Print the array as an argument to PrintArray
-		PrintArray(weekDays);
-		
-		// ChangeArray tries to change the array by assigning something new
-        // to the array in the method.
-		ChangeArray(weekDays);
-		
-		// Print array again after invoking function ChangeArray
-		PrintArray(weekDays);
-		
-		// ChangeArrayElements assigns new values to individual array elements.
-		ChangeArrayElements(weekDays);
-		
-		// Print array again after invoking function ChangeArrayElement
-		PrintArray(weekDays);
 	}
 }
